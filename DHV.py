5db6c5b5ea35d4ec3e78e326ad4a98e116df29f7
P S Sam <87634326+sam7592SAM@users.noreply.github.com>
2023-05-05 20:46:53 +0100

    Update DHV.py

--- DHV.py
+++ DHV.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
@@ -6,85 +7,53 @@ import seaborn as sns
 data = pd.read_csv('laptops_test.csv')
 # print (data.dtypes)
 
-# Filter by Category and Manufacturers
-categories1= ['Gaming']
-manufacturers1 = ['Asus', 'Dell', 'MSI', 'Lenovo']
-# price_range = (data["Price"]<=3600000 ) & (data["Price"] >= 1000000)
+fig = plt.figure(figsize=(20, 18))
+grid = gridspec.GridSpec(3, 2)
 
+# Code to plot a Pie chart
+ax1 = fig.add_subplot(grid[0, 0])
+categories1 = ["Gaming"]
+manufacturers1 = ["Asus", "Dell", "MSI", "Lenovo"]
+filt_1 = data[data["Category"].isin(categories1) & data["Manufacturer"].isin
+              (manufacturers1)]
+manufacturer_counts = filt_1["Manufacturer"].value_counts()
+ax1.pie(manufacturer_counts, labels=manufacturer_counts.index,
+        autopct="%1.1f%%")
+ax1.set_title("Gaming Laptops", weight="bold")
+ax1.legend(loc='best')
 
-filt_1 = data[data['Category'].isin(categories1)
-                     & data['Manufacturer'].isin(manufacturers1)]
+# Create the histogram subplot
+ax2 = fig.add_subplot(grid[1, 1])
 
-'''
-# Drop specified columns
-columns_to_drop = ['Model Name', 'Screen Size', 'Screen', 'CPU', 'GPU', 'Operating System', 'Operating System Version', 'Weight']
-filt_1 = filt_1.drop(columns_to_drop, axis=1)
-'''
+# Filter by Storage
+filt_storage = data[data['Storage'] == '512GB SSD']
 
-# Plot the pie chart
-manufacturer_counts = filt_1['Manufacturer'].value_counts()
+# Extract the 'Price' column
+price_data = filt_storage['Price']
 
-plt.figure(figsize=(8, 6))
-plt.pie(manufacturer_counts, labels=manufacturer_counts.index,
-        autopct='%2.1f%%')
-plt.title('Gaming Laptops', fontsize=15, weight="bold")
-plt.axis('equal')
-plt.legend()
-plt.show()
-
-
-# To plot Line Graph
-# Define the filter conditions
-manufacturers = ['Asus', 'Dell', 'HP']
-category = 'Notebook'
-
-# Apply the filters
-filtered_data = data[
-    (data['Manufacturer'].isin(manufacturers)) &
-    (data['Category'] == category)]
-
-# Group the data by Manufacturer and calculate the average price
-manufacturer_prices = filtered_data.groupby('Manufacturer')['Price'].mean().sort_values()
-
-# Plot the line graph
-plt.figure(figsize=(10, 6))
-plt.plot(manufacturer_prices.index, manufacturer_prices.values, marker='o')
-plt.xlabel('Manufacturer')
-plt.ylabel('Average Price')
-plt.title('Average Price of Notebooks by Manufacturer')
-plt.xticks(rotation=45)
-# plt.grid(True)
-plt.show()
-
+# Plot the histogram
+ax2.hist(price_data, bins=10, edgecolor='black', color='yellow')
+ax2.set_xlabel('Price')
+ax2.set_ylabel('Frequency')
+ax2.set_title('Distribution of Laptop Prices for Storage = 512GB SSD')
 
 # Bar graph
-# Define the filter conditions
+ax3 = fig.add_subplot(grid[1, 0])
 manufacturers3 = ['Dell', 'HP', 'Asus', 'Lenovo', 'MSI']
 cpu = 'Intel Core i7 7700HQ 2.8GHz'
-
-# Apply the filters
-filt_3 = data[
-    (data['Manufacturer'].isin(manufacturers3)) &
-    (data['CPU'] == cpu)
-]
-
-# Group the data by Manufacturer and calculate the average price
-manufacturer_prices = filt_3.groupby('Manufacturer')['Price'].mean().sort_values()
-
+filt_3 = data[(data['Manufacturer'].isin(manufacturers3))
+              & (data['CPU'] == cpu)]
+manufacturer_prices = filt_3.groupby(
+    'Manufacturer')['Price'].mean().sort_values()
 color_palette = ['blue', 'green', 'orange', 'red', 'brown']
+ax3.bar(manufacturer_prices.index,
+        manufacturer_prices.values, color=color_palette)
+ax3.set_xlabel('Manufacturer')
+ax3.set_ylabel('Average Price')
+ax3.set_title('Average Price of Laptops with Highest CPU spec', weight='bold')
 
-# Plot the bar graph
-plt.figure(figsize=(10, 6))
-plt.bar(manufacturer_prices.index, manufacturer_prices.values,
-        color=color_palette)
-plt.xlabel('Manufacturer')
-plt.ylabel('Average Price')
-plt.title('Average Price of Laptops with Highest CPU spec', weight="bold")
-plt.xticks(rotation=45)
-# plt.grid(True)
+# Adjust the layout and spacing
+fig.tight_layout()
+
+# Show the plot
 plt.show()
-
-cpu = data[['CPU','Manufacturer']].groupby('CPU').agg('count').sort_values(
-    'Manufacturer', ascending=False).reset_index()
-sns.barplot(data=cpu.head(10), x="Manufacturer", y="CPU")
-plt.title("CPU vs Total Count of Manufacturers", weight="bold")
